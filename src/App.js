5d7c6a170ac78ae84ee2ffa962aa8bb5a7b9a8cb
sder12 <sara.cetto@gmail.com>
2023-03-30 19:26:52 +0200

    Save data in variable + debug foreach

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Article from './Article';
+import data from './data';
 
 const App = () => {
+  const [articles, setArticles] = useState(data);
+  console.log(articles)
+
+
+
   return (
     <section className="section-center">
       <div className="container">
         <h2>DARK MODE APP</h2>
+        <button type="button" className='btn'>CHANGE</button>
+        {
+          articles.map((el) => {
+            return <Article />
+          })
+        }
       </div>
     </section>
   )
---
+++ src/Article.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Article = () => {
+    return (
+        <article>
+            <h5>Article</h5>
+        </article>
+    )
+}
+
+export default Article
